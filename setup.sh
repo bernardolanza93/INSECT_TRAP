168ab4569cc5af8c3413487666104cb85a2b01eb
bernardolanza93 <bernardo.lanza.tech@gmail.com>
2022-10-03 16:17:36 +0200

    added setup

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#upgrade pip
+python -m pip install --upgrade pip
+pip install -U pip
+sudo apt-get update
+sudo apt-get upgrade
+pip install upgrade numpy
+sudo apt install libatlas-base-dev
+sudo apt-get install git
+git clone
+
+
+sudo apt-get install libopencv-dev python3-opencv-contrib #rivedi
