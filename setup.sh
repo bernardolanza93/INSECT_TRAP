407669e03f55fd0dbac6d09dc05db02c2861d6fc
bernardolanza93 <bernardo.lanza.tech@gmail.com>
2022-10-03 16:25:27 +0200

    update setup

--- setup.sh
+++ setup.sh
@@ -7,11 +7,13 @@ pip install upgrade numpy
 sudo apt install libatlas-base-dev
 sudo apt-get install git
 cd
-git clone https://github.com/bernardolanza93/INSECT_TRAP.git
+#git clone https://github.com/bernardolanza93/INSECT_TRAP.git
 cd
 cd INSECT_TRAP/
 git pull
 cd
+
+sudo apt-get install libhdf5-dev libhdf5-serial-dev
 
 
 sudo apt-get install libopencv-dev python3-opencv-contrib #rivedi
