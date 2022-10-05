c62b6bffb1683ba82db849ab46c42866532e7f4c
bernardolanza93 <bernardo.lanza.tech@gmail.com>
2022-10-05 17:11:32 +0200

    button checker update

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ Low profile design to fit inside lots of existing Raspberry Pi cases!
 
 
 AUTO_POWER_OFF = False
+BUTTON_PIN = 10
 PINOUT = 1
 
 def send_signal_2_arduino(PIN):
@@ -52,13 +53,14 @@ def blink_led(PIN,blink_time):
 def check_button_pressure():
     GPIO.setwarnings(False)  # Ignore warning for now
     GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
-    GPIO.setup(10, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(BUTTON_PIN, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
     # Set pin 10 to be an input pin and set initial value to be pulled low (off)
     start = time.time()
     while time < 5:  # Run forever
         stop = time.time()
         time = stop-start
-        if GPIO.input(10) == GPIO.HIGH:
+        buttonState = GPIO.input(BUTTON_PIN)
+        if buttonState == False:
             print("Button was pushed!")
         else:
             blink_led(2,0.2)
