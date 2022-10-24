eeac960f54a002463fa6192aca300a7705385046
bernardolanza93 <bernardo.lanza.tech@gmail.com>
2022-10-24 12:16:09 +0200

    setup

--- setup.sh
+++ setup.sh
@@ -8,13 +8,14 @@ sudo apt install libatlas-base-dev
 sudo apt-get install git
 sudo apt-get install rpi.gpio
 pip install multiprocess
-cd
+sudo apt-get install build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103 python3-pyqt5 python3-dev -y
+#cd
 #git clone https://github.com/bernardolanza93/INSECT_TRAP.git
-#
-cd
-cd INSECT_TRAP/
-git pull
-cd
+##
+#cd#
+#cd INSECT_TRAP/
+#git pull
+#cd
 
 sudo apt-get install libhdf5-dev libhdf5-serial-dev
 sudo pip install telepot
